759b08d5268ddcc5498e5ff5959c95f9ec865a2c
Victor Koronen <koronen@kth.se>
2011-01-09 15:52:27 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spargris::Application.config.secret_token = 'f0b132ffccc407f518688fb9b5ad01867d1b5da637f99ef327a2e5c729e015d21f09226d1080e9b2b010dd41a30f94758ed80b27b14a8aca90447230183b075f'
