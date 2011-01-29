6817f16a028d69136e3a198289bf977a34264451
Victor Koronen <koronen@kth.se>
2011-01-30 00:06:27 +0100

    Added basic swedish i18n.

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Spargris
     config.time_zone = 'Stockholm'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :se
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :sv
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
