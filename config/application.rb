d05f2a8681f2164733517a0d8f2c9864b4584038
Victor Koronen <koronen@kth.se>
2011-07-01 00:03:00 +0200

    Use HAML as template engine

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,12 @@ module Spargris
 
     #Encoding.default_external = Encoding::UTF_8
     #Encoding.default_internal = Encoding::UTF_8
+
+    # Specify generators
+    config.generators do |g|
+      g.template_engine :haml
+      #g.test_framework :rspec, :fixture => true, :views => false
+      #g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
