8dea196bbec2a240883888320daa5e8cb281cd2d
Victor Koronen <koronen@kth.se>
2011-07-01 00:14:17 +0200

    Add title helpers and use them for budgets and budget posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,26 @@
 module ApplicationHelper
+  def build_title
+    title = ""
+    cf = content_for(:title)
+    unless cf.empty?
+      title += "#{cf} - "
+    end
+    title += "Spargris"
+  end
+
+  def index_title(klass)
+    klass.human_name.pluralize
+  end
+  def show_title(resource)
+    "#{resource.class.human_name} ##{resource.id}"
+  end
+  def new_title(resource)
+    t('helpers.submit.create', :model => resource.class.human_name.downcase)
+  end
+  def edit_title(resource)
+    t('helpers.submit.update', :model => resource.class.human_name.downcase) + " ##{resource.id}"
+  end
+
   def mark_active_tab(resource)
     " active" if active_tab?(resource)
   end
