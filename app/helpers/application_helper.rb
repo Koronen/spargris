d6e464c121fdedf3a5d0e74362feb16e123d0bb5
Victor Koronen <koronen@kth.se>
2011-07-05 23:12:58 +0200

    Move modification links into a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,6 +30,16 @@ module ApplicationHelper
     end
   end
 
+  def modification_image_links(resource)
+    output = []
+    singular_class_name = resource.class.name.downcase
+    output << link_to(image_tag("icons/zoom.png", :alt => "Show #{singular_class_name}", :title => "Show #{singular_class_name}"), resource)
+    output << link_to(image_tag("icons/wrench.png", :alt => "Edit #{singular_class_name}", :title => "Edit #{singular_class_name}"), method("edit_#{singular_class_name}_path").call(resource))
+    output << link_to(image_tag("icons/page_copy.png", :alt => "Clone #{singular_class_name}", :title => "Clone #{singular_class_name}"), method("new_#{singular_class_name}_path").call(:template => resource))
+    output << link_to(image_tag("icons/delete.png", :alt => "Delete #{singular_class_name}", :title => "Delete #{singular_class_name}"), resource, :confirm => 'Are you sure?', :method => :delete)
+    output.join(" ").html_safe
+  end
+
   def mark_active_tab(resource)
     " active" if active_tab?(resource)
   end
