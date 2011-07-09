46e7d61bc2236b521cd307b89a97227957632a8d
Victor Koronen <koronen@kth.se>
2011-07-09 14:24:20 +0200

    Fix deprecation in edit links helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,20 +9,20 @@ module ApplicationHelper
   end
 
   def index_title(klass)
-    klass.human_name.pluralize
+    klass.model_name.human.pluralize
   end
   def show_title(resource)
     if resource.has_attribute? :name
-      "#{resource.class.human_name}: #{resource.name}"
+      "#{resource.class.model_name.human}: #{resource.name}"
     else
-      "#{resource.class.human_name} ##{resource.id}"
+      "#{resource.class.model_name.human} ##{resource.id}"
     end
   end
   def new_title(resource)
-    t('helpers.submit.create', :model => resource.class.human_name.downcase)
+    t('helpers.submit.create', :model => resource.class.model_name.human.downcase)
   end
   def edit_title(resource)
-    edit_text = t('helpers.submit.update', :model => resource.class.human_name.downcase)
+    edit_text = t('helpers.submit.update', :model => resource.class.model_name.human.downcase)
     if resource.has_attribute? :name
       "#{edit_text}: #{resource.name}"
     else
@@ -32,11 +32,12 @@ module ApplicationHelper
 
   def modification_image_links(resource)
     output = []
-    singular_class_name = resource.class.name.downcase
-    output << link_to(image_tag("icons/zoom.png", :alt => "Show #{singular_class_name}", :title => "Show #{singular_class_name}"), resource)
-    output << link_to(image_tag("icons/wrench.png", :alt => "Edit #{singular_class_name}", :title => "Edit #{singular_class_name}"), method("edit_#{singular_class_name}_path").call(resource))
-    output << link_to(image_tag("icons/page_copy.png", :alt => "Clone #{singular_class_name}", :title => "Clone #{singular_class_name}"), method("new_#{singular_class_name}_path").call(:template => resource))
-    output << link_to(image_tag("icons/delete.png", :alt => "Delete #{singular_class_name}", :title => "Delete #{singular_class_name}"), resource, :confirm => 'Are you sure?', :method => :delete)
+    singular_table_name = resource.class.table_name.singularize
+    singular_human_name = resource.class.model_name.human.downcase
+    output << link_to(image_tag("icons/zoom.png", :alt => "Show #{singular_human_name}", :title => "Show #{singular_human_name}"), resource)
+    output << link_to(image_tag("icons/wrench.png", :alt => "Edit #{singular_human_name}", :title => "Edit #{singular_human_name}"), method("edit_#{singular_table_name}_path").call(resource))
+    output << link_to(image_tag("icons/page_copy.png", :alt => "Clone #{singular_human_name}", :title => "Clone #{singular_human_name}"), method("new_#{singular_table_name}_path").call(:template => resource))
+    output << link_to(image_tag("icons/delete.png", :alt => "Delete #{singular_human_name}", :title => "Delete #{singular_human_name}"), resource, :confirm => 'Are you sure?', :method => :delete)
     output.join(" ").html_safe
   end
 
