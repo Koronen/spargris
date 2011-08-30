0c37fb02408805bb81eb3a492e1c4f9c70948bb7
Victor Koronen <koronen@kth.se>
2011-08-30 13:12:34 +0200

    Don't allow null

--- db/migrate/20110129195836_create_accounts.rb
+++ db/migrate/20110129195836_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration
   def self.up
     create_table :accounts do |t|
-      t.string :name
+      t.string :name, :null => false
 
       t.timestamps
     end
