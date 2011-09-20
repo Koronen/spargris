b2b0cae9b680d64ca67c6d23f144e76c6f0811cd
Victor Koronen <koronen@kth.se>
2011-09-20 12:20:41 +0200

    Add budget model spec stub

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
 
 describe Budget do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "attr_accessible :starts_at, :ends_at, :budget_items_attributes"
+    it "accepts_nested_attributes_for :budget_items"
+    it "validates_presence_of :user, :starts_at, :ends_at"
+  end
+
+  describe "scopes" do
+    it "default scope"
+    it "scope :current"
+  end
+
+  describe "days" do
+    it "#days" do
+      budget = Budget.make :starts_at => 1.day.ago, :ends_at => 1.day.from_now
+      budget.days.should be_within(0.01).of(2.0)
+    end
+
+    it "#days_gone" do
+      budget = Budget.make :starts_at => 1.day.from_now, :ends_at => 3.days.from_now
+      budget.days_gone.should eq(0)
+
+      budget = Budget.make :starts_at => 1.day.ago, :ends_at => 1.day.from_now
+      budget.days_gone.should be_within(0.01).of(1.0)
+
+      budget = Budget.make :starts_at => 3.days.ago, :ends_at => 1.day.ago
+      budget.days_gone.should be_within(0.01).of(2.0)
+    end
+
+    it "#days_left" do
+      budget = Budget.make :starts_at => 1.day.from_now, :ends_at => 3.days.from_now
+      budget.days_left.should be_within(0.01).of(2.0)
+
+      budget = Budget.make :starts_at => 1.day.ago, :ends_at => 1.day.from_now
+      budget.days_left.should be_within(0.01).of(1.0)
+
+      budget = Budget.make :starts_at => 3.days.ago, :ends_at => 1.day.ago
+      budget.days_left.should eq(0)
+    end
+  end
+
+  it "amount"
+
+  it "sum"
+
+  it "spent"
+
+  it "earned"
+
+  it "difference"
+
+  it "margin"
+
+  it "spent_per_day"
+
+  it "earned_per_day"
+
+  it "difference_per_day"
+
+  it "margin_per_day"
 end
