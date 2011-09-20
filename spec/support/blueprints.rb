1a335ca7e7d0a4f3f384e16d7450b247ed52e5d2
Victor Koronen <koronen@kth.se>
2011-09-20 10:54:35 +0200

    Generate model specs and blueprints

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -7,3 +7,31 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+BudgetItem.blueprint do
+  # Attributes here
+end
+
+BudgetPost.blueprint do
+  # Attributes here
+end
+
+Budget.blueprint do
+  # Attributes here
+end
+
+Transaction.blueprint do
+  # Attributes here
+end
+
+TransactionItem.blueprint do
+  # Attributes here
+end
+
+User.blueprint do
+  # Attributes here
+end
+
+Vendor.blueprint do
+  # Attributes here
+end
