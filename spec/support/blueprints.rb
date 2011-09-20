84019f60e260897e8c0af962d2253e8099c9c773
Victor Koronen <koronen@kth.se>
2011-09-20 11:52:31 +0200

    Fill in blueprints

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,37 +1,46 @@
 require 'machinist/active_record'
 
-# Add your blueprints here.
-#
-# e.g.
-#   Post.blueprint do
-#     title { "Post #{sn}" }
-#     body  { "Lorem ipsum..." }
-#   end
-
 BudgetItem.blueprint do
-  # Attributes here
+  budget
+  budget_post
+  amount { sn.to_i % 1000 }
 end
 
 BudgetPost.blueprint do
-  # Attributes here
+  user
+  name { "BP#{sn}" }
+  description { "Desc#{sn}" }
+  parent { nil }
 end
 
 Budget.blueprint do
-  # Attributes here
+  user
+  starts_at { 2.weeks.ago }
+  ends_at { 2.weeks.from_now }
 end
 
 Transaction.blueprint do
-  # Attributes here
+  user
+  timestamp { 1.day.ago }
+  vendor
+  description { "Desc#{sn}" }
 end
 
 TransactionItem.blueprint do
-  # Attributes here
+  transaction
+  budget_post
+  amount { sn.to_i % 1000 }
+  description { "Desc#{sn}" }
 end
 
 User.blueprint do
-  # Attributes here
+  username { "user#{sn}" }
+  email { "sample@example.com" }
+  time_zone { "London" }
+  language { "en" }
 end
 
 Vendor.blueprint do
-  # Attributes here
+  user
+  name { "Vendor#{sn}" }
 end
